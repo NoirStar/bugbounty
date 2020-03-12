7b20d38e166d07bcfc34d248fab455e06b9af45d
NoirStar <sky_9233@naver.com>
2020-03-12 23:32:18 +0900

    attack edit

--- find.sh
+++ find.sh
@@ -36,9 +36,14 @@ do
     # massdns 
     cat subdomainAll_"$URI".txt | massdns -r "$TOOLS"/datafile/resolvers.txt -t A -o S -w massdns_"$URI".txt;
     sed 's/A.*//' massdns_"$URI".txt | sed 's/CN.*//' | sed 's/\..$//' > sed_"$URI".txt;
-    cat sed_"$URI".txt | httprobe -c 40 -t 3000 -p http:8443 -p https:8443 -p http:8080 -p https:8 080 -p http:8008 -p https:8008 -p http:591 -p https:591 -p http:593 -p https:593 -p http:981 -p https:981 -p http:2480 -p https:2480 -p http:4567 -p https:4567 -p http:5000 -p https:5000 -p http:5800 -p https:5800 -p http:7001 -p https:7001 -p http:7002 -p https:7002 -p http:9080 -p https:9080 -p http:9090 -p https:9090 -p https:9443 -p https:18091 -p https:18092 | tee final_"$URI".txt;
+    cat sed_"$URI".txt | httprobe -c 40 -t 3000 -p http:8443 -p https:8443 -p http:8080 -p https:8 080 -p http:8008 -p https:8008 -p http:591 -p https:591 -p http:593 -p https:593 -p http:981 -p https:981 -p http:2480 -p https:2480 -p http:4567 -p https:4567 -p http:5000 -p https:5000 -p http:5800 -p https:5800 -p http:7001 -p https:7001 -p http:7002 -p https:7002 -p http:9080 -p https:9080 -p http:9090 -p https:9090 -p https:9443 -p https:18091 -p https:18092 | tee httprobe_"$URI".txt;
+    cat httprobe"$URI".txt | sort | uniq > final_"$URI".txt;
     wc -l final_"$URI".txt;
 
+    # whatweb
+    whatweb -v -a 3 --input-file=final_"$URI".txt | tee whatweb_"$URI"_result.txt;
+
+    # gowitness
     mkdir -pv /Screenshots;
     gowitness file --source=./final_"$URI".txt --threads=8 --resolution="1200,750" --log-format=json --log-level=warn --timeout=3 --destination="./Screenshots";
 
